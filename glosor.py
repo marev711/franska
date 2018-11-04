74b49761fc980b168dc46c84c09ab1cb15c29567
Martin Evaldsson <mevaldsson@gmail.com>
2018-11-04 13:00:27 +0100

    Using shuffle for memory performance

--- glosor.py
+++ glosor.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 import random
 import itertools
-swedish  =  ["var",  "nu",           "synd",     "jag vinner",  "en blomma",    "ett träd",  "en man",    "en motocykel"]
-french   =  ["où",   "maintenant",   "dommage",  "je gagne",    "une fleur",    "un arbre",  "un homme",  "une moto"]
+swedish  =  ["blå",  "vit",   "röd",   "gul",   "grön", "svart", "brun",   "brun (ögon)", "lila",  "turkos",   "beige", "grå",  "orange", "rosa", "mörkgrön",   "ljusblå"]
+french   =  ["bleu", "blanc", "rouge", "jaune", "vert", "noir",  "marron", "brun",        "lilas", "turquois", "beige", "gris", "orange", "rose", "vert foncé", "bleu clair"]
 len_french=len(french)
 index_french=list(range(len_french))
-my_permutations=list(itertools.permutations(index_french))
-len_myperm=len(my_permutations)
-my_permutation=my_permutations[random.randint(0, len_myperm-1)]
-print(my_permutation)
+random.shuffle(index_french)
 
-for index in my_permutation:
+for index in index_french:
   Answer=input('what is the french for: ' + swedish[index]+"   ")
   if Answer==french[index]:
    print("Correct!")
@@ -19,4 +16,7 @@ for index in my_permutation:
 
 #english  =  ["zero",  "one",  "two",   "three",  "four",    "five",  "six",  "seven",  "eight",  "nine",  "ten",  "eleven",  "twelve",  "thirteen",  "fourteen",  "fifteen",  "sixteen",  "seventeen",  "eighteen",  "nineteen",  "twenty",  "twenty-one",  "twenty-two",  "twenty-three",  "twenty-four",   "twenty-five",  "twenty-six",  "twenty-seven",  "twenty-eight",  "twenty-nine",  "thirty",  "fourty",    "fifty",      "sixty",     "seventy",       "eighty",        "ninety",            "hundred",  "thousand"]
 #french   =  ["zéro",  "un",   "deux",  "trois",  "quatre",  "cinq",  "six",  "sept",   "huit",   "neuf",  "dix",  "onze",    "douze",   "treize",    "quatorze",  "quinze",   "seize",    "dix-sept",   "dix-huit",  "dix-neuf",  "vingt",   "vingt",       "vingt-deux",  "vingt-trois",   "vingt-quatre",  "vingt-cinq",   "vingt-six",   "vingt-sept",    "vingt-huit",    "vingt-neuf",   "trente",  "quarante",  "cinquante",  "soixante",  "soixante-dix",  "quatre-vingt",  "quatre-vingt-dix",  "cent",     "mille"]
+
+swedish  =  ["var",  "nu",           "synd",     "jag vinner",  "en blomma",    "ett träd",  "en man",    "en motocykel"]
+french   =  ["où",   "maintenant",   "dommage",  "je gagne",    "une fleur",    "un arbre",  "un homme",  "une moto"]
 
