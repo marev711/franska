337d82d956a9eba6bafb2c94c6fc24618d96d3cf
Martin Evaldsson <mevaldsson@gmail.com>
2018-11-17 18:43:07 +0100

    Added next week's homework

--- glosor.py
+++ glosor.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+import pdb
 import random
 import itertools
-swedish  =  ["blå",  "vit",   "röd",   "gul",   "grön", "svart", "brun",   "brun (ögon)", "lila",  "turkos",   "beige", "grå",  "orange", "rosa", "mörkgrön",   "ljusblå"]
-french   =  ["bleu", "blanc", "rouge", "jaune", "vert", "noir",  "marron", "brun",        "lilas", "turquois", "beige", "gris", "orange", "rose", "vert foncé", "bleu clair"]
+swedish  =  ["en ko",     "en gris"]#,   "en kanin", "ett marsvin",       "en get",     "en hamster", "en häst",   "en apa",   "en papegoja",  "en katt", "en mus",     "en tiger", "en haj",    "en råtta", "en näbbmus",     "djur"]
+french   =  ["une vache", "un cochon"]#, "un lapin", "un cochon d'Inde", "une chèvre", "un hamster", "un cheval", "un singe", "un perroquet", "un chat", "une souris", "un tigre", "un requin", "un rat",   "une musaraigne", "animaux"]
 len_french=len(french)
 index_french=list(range(len_french))
+fel = []
 random.shuffle(index_french)
 
 for index in index_french:
@@ -12,8 +14,13 @@ for index in index_french:
   if Answer==french[index]:
    print("Correct!")
   else:
+   fel.append(french[index])
    print("Wrong '" + french[index] + "' is the correct answer.")
 
+pdb.set_trace()
+
+#swedish  =  ["blå",  "vit",   "röd",   "gul",   "grön", "svart", "brun",   "brun (ögon)", "lila",  "turkos",   "beige", "grå",  "orange", "rosa", "mörkgrön",   "ljusblå"]
+#french   =  ["bleu", "blanc", "rouge", "jaune", "vert", "noir",  "marron", "brun",        "lilas", "turquois", "beige", "gris", "orange", "rose", "vert foncé", "bleu clair"]
 #english =  ["zero",  "one",  "two",   "three",  "four",    "five",  "six",  "seven",  "eight",  "nine",  "ten",  "eleven",  "twelve",  "thirteen",  "fourteen",  "fifteen",  "sixteen",  "seventeen",  "eighteen",  "nineteen",  "twenty",  "twenty-one",  "twenty-two",  "twenty-three",  "twenty-four",   "twenty-five",  "twenty-six",  "twenty-seven",  "twenty-eight",  "twenty-nine",  "thirty",  "fourty",    "fifty",      "sixty",     "seventy",       "eighty",        "ninety",            "hundred",  "thousand"]
 #french   =  ["zéro",  "un",   "deux",  "trois",  "quatre",  "cinq",  "six",  "sept",   "huit",   "neuf",  "dix",  "onze",    "douze",   "treize",    "quatorze",  "quinze",   "seize",    "dix-sept",   "dix-huit",  "dix-neuf",  "vingt",   "vingt",       "vingt-deux",  "vingt-trois",   "vingt-quatre",  "vingt-cinq",   "vingt-six",   "vingt-sept",    "vingt-huit",    "vingt-neuf",   "trente",  "quarante",  "cinquante",  "soixante",  "soixante-dix",  "quatre-vingt",  "quatre-vingt-dix",  "cent",     "mille"]
 
